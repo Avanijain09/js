2e83129f659439e2c3bc3127be0bbf8ce19bf9ed
avani <avanijain875@gmail.com>
2025-02-16 14:10:07 +0530

    test done

---
+++ test.js
@@ -0,0 +1,41 @@
+console.log("Avani")
+// in tino se likh skte he variable ko
+let email= "avani789@gmail.com" 
+var pass= "12345"
+city = "jaipur" 
+console.table([email,pass,city])
+
+// data types
+"use strict";  //treat all js code as newer version
+// alert(3+3) we are using nodejs,not browser
+
+// number
+// bigint
+// String
+// boolean
+// null standalone value
+// undefined =value assign nhi hui he
+// non primitive
+// object
+
+console.log(typeof("Avani"))
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+// let score="30a"
+// console.log(typeof score);
+// let value = Number(score)
+// console.log(typeof score);
+let score=null
+console.log(typeof score);
+let value = Number(score)
+console.log(typeof score);
+// "30"=30
+// "33abc"=NaN
+// true=1
+// ""= flase
+console.log(1+"2"+3) //123
+console.log("1"+2+3) //123
+console.log(1+2+"3") //33
+
+
