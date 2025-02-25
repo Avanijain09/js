7149c9eeeb182b6e5faee77d5baa5a1398e9fbdb
avani <avanijain875@gmail.com>
2025-02-25 13:34:38 +0530

    update

--- object.js
+++ object.js
@@ -18,5 +18,36 @@
 // console.log(jsuser.greetingTwo())
  
 // singleton or  constructor
-const a = new Object()
-const a = {}
+const obj = new Object()
+const obj1 = {}
+obj.id = "piet28"
+obj.name = "avani"
+obj.isloggedIn = false
+//console.log(obj)
+//console.log(obj1)
+const b = {
+    email:"avani@gmail.com",
+    fullname:{
+        username:{
+            firstname:"avani",
+            lastname:"jain"
+        }
+    }
+}
+console.log(b.fullname)
+console.log(b.fullname.username.firstname)
+const obj2 = {1:"a",2:"b"}
+const obj3 = {3:"c",4:"d"}
+const obj4 = {obj2,obj3}
+console.log(obj4)
+const obj5 = Object.assign({},obj2,obj3)
+console.log(obj5)
+const obj6 = {...obj2,...obj3}
+console.log(obj6)
+console.log(Object.keys(b))
+console.log(Object.values(b))
+console.log(Object.entries(b))
+
+
+
+console.log(b.hasOwnProperty('isLogged'))
